b89e340ebd824f132b2605f5d2f33076e04020ba
dxy159 <richard.ni.wchs@gmail.com>
2017-01-10 21:53:35 -0500

    found accuracy of forecasting data

--- regression.py
+++ regression.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import quandl, math
 import numpy as np
-from sklearn import preprocessing, cross_validation, svm
-from sklearn.linear_model import linearRegression
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -15,18 +15,20 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace = True)
 
 forecast_out = int(math.ceil(0.01 * len(df)))
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace = True)
-print(df.head())
-
 
 X = np.array(df.drop(['label'], 1))
 y = np.array(df['label'])
-
 X = preprocessing.scale(X)
-
-df.dropna(inplace = True)
 y = np.array(df['label'])
 
-print(len(X), len(y))
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
