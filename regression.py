bbb6103ed98cef5d77183d3fdf7e4f2121d8ddf3
dxy159 <richard.ni.wchs@gmail.com>
2017-01-11 20:30:49 -0500

    half way through part 5

--- regression.py
+++ regression.py
@@ -1,8 +1,12 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -18,11 +22,14 @@ forecast_out = int(math.ceil(0.01 * len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace = True)
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace = True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
@@ -31,4 +38,14 @@ clf = LinearRegression()
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_out)
+
+
+
+
+
+
+
+
