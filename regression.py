9bbac4a35b0835b6ca5198eb24c2daca7dbce661
dxy159 <richard.ni.wchs@gmail.com>
2017-01-18 22:24:31 -0500

    added comments

--- regression.py
+++ regression.py
@@ -13,12 +13,14 @@ df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
+# WHAT HINGES ON PRICE?
+#           price         x           x              x
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace = True)
 
-forecast_out = int(math.ceil(0.01 * len(df)))
+forecast_out = int(math.ceil(0.1 * len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
